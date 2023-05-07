7e395d14ea9db6dbafd5f0c1923dd1d7e9a61ad6
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-05-07 14:54:24 -0400

    routenando las rutas con url for . luego toca cambiar las imgaenes y los links de las redes sociales

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -8,4 +8,4 @@ app = Flask(__name__)
 def index():
     
     
-    pass
+    return render_template('index.html')
