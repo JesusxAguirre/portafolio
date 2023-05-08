08873502a0631a8418a285db30f9a01652c78e19
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-05-08 15:59:14 -0400

    Agregando libreria de flask-Mail para que puedan comunicarse en mi portafolio conmig

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, url_for
-
+from flask_mail import Mail
 app = Flask(__name__)
 
+app.config['MAIL_SERVER']= 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] ='quijess6@gmail.com'
+app.config['MAIL_PASSWORD'] = 'onebzquvinbjzbat'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] =True
 
+Mail(app)
 
 @app.route("/")
 def index():
